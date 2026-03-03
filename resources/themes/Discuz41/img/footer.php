4f273e9b8329574c1205cb35f9d99a2e9b067c71
sumitid <host@442442.xyz>
2026-03-03 11:50:49 +0800

    Add files via upload

---
+++ resources/themes/Discuz41/img/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <p>© 2006-<?php echo date('Y'); ?> Powered by DirectoryLister · Discuz!4.1.0 经典主题 © 凡星儿 设计</p>
+    <p>
+        <a href="#">关于本站</a> | 
+        <a href="#">下载帮助</a> | 
+        <a href="#">问题反馈</a> | 
+        <a href="https://github.com/sumitid/DirectoryLister" target="_blank">项目主页</a>
+    </p>
+</footer>
