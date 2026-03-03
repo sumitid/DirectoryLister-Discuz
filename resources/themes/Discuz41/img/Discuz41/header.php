92b0b78f9033c3578b3c149599727680df1a6804
sumitid <host@442442.xyz>
2026-03-03 12:21:26 +0800

    Delete resources/themes/Discuz41/img directory

--- resources/themes/Discuz41/img/Discuz41/header.php
+++
@@ -1,4 +0,0 @@
-<meta name="keywords" content="软件下载,开源软件,免费软件,绿色软件,技术博客,云盘下载,系统工具,网络软件,编程开发,DirectoryLister,Discuz主题">
-<meta name="description" content="DirectoryLister网盘Discuz!4.1.0主题 - 专注于分享优质软件和技术资源，本站提供各类软件下载、技术文档和开源项目。所有软件均为绿色版，放心下载。">
-<meta name="author" content="DirectoryLister">
-<meta name="robots" content="index,follow">
